eb2e7b01fee8dad834dec7411c3b3ddbba1100cf
Will Bamford <w.bamford@gmail.com>
2017-04-07 09:48:52 +0100

    Quick bit of styling

--- src/components/Timeline/Timebar/Cell.jsx
+++ src/components/Timeline/Timebar/Cell.jsx
@@ -1,9 +1,9 @@
 import React, { PropTypes } from 'react'
 
-const Cell = ({ time, id, title, start }) =>
+const Cell = ({ time, id, title, start, end }) =>
   <div
     className="timebar__cell"
-    style={time.toStyleLeft(start)}
+    style={time.toStyleLeftAndWidth(start, end)}
   >
     {title}
   </div>
@@ -12,7 +12,8 @@ Cell.propTypes = {
   time: PropTypes.shape({}),
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  start: PropTypes.instanceOf(Date).isRequired
+  start: PropTypes.instanceOf(Date).isRequired,
+  end: PropTypes.instanceOf(Date).isRequired
 }
 
 export default Cell
