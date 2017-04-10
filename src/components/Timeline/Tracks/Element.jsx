acd874079c1872b87ba92641fbd1b2c89aebed35
Will Bamford <w.bamford@gmail.com>
2017-04-10 18:04:50 +0100

    WIP changes

--- src/components/Timeline/Tracks/Element.jsx
+++ src/components/Timeline/Tracks/Element.jsx
@@ -1,14 +1,16 @@
 import React, { PropTypes } from 'react'
 import BasicElement from '../../Elements/Basic'
 
-const Element = ({ time, parentStyle, id, title, start, end }) => {
+const Element = ({ time, parentStyle, style, id, title, start, end }) => {
+  console.log('style', style)
+  console.log('parentStyle', parentStyle)
   return (
     <div
       key={id}
       className="track__element"
       style={time.toStyleLeftAndWidth(start, end)}
     >
-      <BasicElement title={title} style={parentStyle} />
+      <BasicElement title={title} style={{ ...parentStyle, ...style }} />
     </div>
   )
 }
@@ -16,6 +18,7 @@ const Element = ({ time, parentStyle, id, title, start, end }) => {
 Element.propTypes = {
   time: PropTypes.shape({}).isRequired,
   parentStyle: PropTypes.shape({}),
+  style: PropTypes.shape({}),
   id: PropTypes.string.isRequired,
   title: PropTypes.string,
   start: PropTypes.instanceOf(Date),
