7cd4ae75576edf6de9e1b0bcfd1566e3c7002102
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-19 11:51:54 +0100

    Add marker abstraction with TodayMarker

--- src/components/Timeline/Body.jsx
+++ src/components/Timeline/Body.jsx
@@ -1,16 +1,12 @@
 import React, { PropTypes } from 'react'
 import Tracks from './Tracks'
-import Now from './Now'
-import datePropType from '../../utils/datePropType'
 
-const Body = ({ now, time, tracks }) =>
+const Body = ({ time, tracks }) =>
   <div className="timeline__body">
     <Tracks time={time} tracks={tracks} />
-    { now && <Now now={now} time={time} /> }
   </div>
 
 Body.propTypes = {
-  now: datePropType,
   time: PropTypes.shape({}).isRequired,
   tracks: PropTypes.arrayOf(PropTypes.shape({}))
 }
---
+++ src/utils/getMonth.js
@@ -0,0 +1,5 @@
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+const getMonth = date => monthNames[date.getMonth()]
+
+export default getMonth
