3e4125fec853f25713849d5126ebca6b4de3f285
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-19 13:31:14 +0100

    Add sidebar header keys

--- src/components/Sidebar/Header.jsx
+++ src/components/Sidebar/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () =>
   <div className="sidebar__header">
-    <div className="timebar-key" />
-    <div className="timebar-key" />
-    <div className="timebar-key" />
+    <div className="timebar-key">Quarters</div>
+    <div className="timebar-key">Periods</div>
+    <div className="timebar-key">Months</div>
   </div>
 
 export default Header
