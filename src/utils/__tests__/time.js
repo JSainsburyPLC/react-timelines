52babfb21ef6291a5b833ec0f5091353c883a847
Will Bamford <w.bamford@gmail.com>
2017-04-20 11:35:55 +0100

    Fix unit tests

--- src/utils/__tests__/time.js
+++ src/utils/__tests__/time.js
@@ -6,7 +6,7 @@ describe('createTime', () => {
       const { timelineWidth } = createTime({
         start: new Date('2017-01-01T00:00:00.000Z'),
         end: new Date('2018-01-01T00:00:00.000Z'),
-        scale: 10 // 10px === 1 day
+        factor: 10 // 10px === 1 day
       })
       expect(timelineWidth).toBe(3650)
     })
@@ -17,7 +17,7 @@ describe('createTime', () => {
       const { timelineWidth } = createTime({
         start: newYear,
         end: newYearMidday,
-        scale: 100
+        factor: 100
       })
       expect(timelineWidth).toBe(50)
     })
@@ -27,7 +27,7 @@ describe('createTime', () => {
     it('calculates correct x pixel position for given date', () => {
       const start = new Date('2017-01-01T00:00:00.000Z')
       const end = new Date('2018-01-01T00:00:00.000Z')
-      const { toX } = createTime({ start, end, scale: 2 })
+      const { toX } = createTime({ start, end, factor: 2 })
       const nearMiddle = new Date('2017-07-01')
       const notClamped = new Date('2020-01-01')
       expect(toX(end)).toBe(730)
@@ -42,7 +42,7 @@ describe('createTime', () => {
       const start = new Date('2017-01-01T00:00:00.000Z')
       const firstOfJune = new Date('2017-06-01T12:34:56.000Z')
       const end = new Date('2018-01-01T00:00:00.000Z')
-      const { toStyleLeft } = createTime({ start, end, scale: 2 })
+      const { toStyleLeft } = createTime({ start, end, factor: 2 })
       expect(toStyleLeft(start)).toEqual({ left: '0px' })
       expect(toStyleLeft(firstOfJune)).toEqual({ left: '303.0485185185185px' })
       expect(toStyleLeft(end)).toEqual({ left: '730px' })
@@ -54,7 +54,7 @@ describe('createTime', () => {
       const start = new Date('2017-01-01T00:00:00.000Z')
       const firstOfJune = new Date('2017-06-01T12:34:56.000Z')
       const end = new Date('2018-01-01T00:00:00.000Z')
-      const { toStyleLeftAndWidth } = createTime({ start, end, scale: 2 })
+      const { toStyleLeftAndWidth } = createTime({ start, end, factor: 2 })
       expect(toStyleLeftAndWidth(start, end)).toEqual({ left: '0px', width: '730px' })
       expect(toStyleLeftAndWidth(firstOfJune, end)).toEqual({ left: '303.0485185185185px', width: '426.9514814814815px' })
     })
