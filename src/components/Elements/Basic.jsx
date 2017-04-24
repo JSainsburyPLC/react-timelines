adbefe48ab39f77808da5e9acc1f6e4e9d3dbaf6
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-24 13:31:29 +0100

    WIP

--- src/components/Elements/Basic.jsx
+++ src/components/Elements/Basic.jsx
@@ -1,14 +1,24 @@
 import React, { PropTypes } from 'react'
 
-const Basic = ({ title, style }) =>
+import datePropType from '../../utils/datePropType'
+import { getDayMonth } from '../../utils/formatDate'
+
+const Basic = ({ title, start, end, style }) =>
   <div className="element" style={style}>
-    <div className="element__content">
+    <div className="element__content" aria-hidden="true">
       { title }
+    </div>
+    <div className="element__tooltip">
+      <div><strong>Title</strong> {title}</div>
+      <div><strong>Start</strong> {getDayMonth(start.toDate())}</div>
+      <div><strong>End Date</strong> {getDayMonth(end.toDate())}</div>
     </div>
   </div>
 
 Basic.propTypes = {
   title: PropTypes.string,
+  start: datePropType,
+  end: datePropType,
   style: PropTypes.shape({})
 }
 
