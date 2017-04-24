d036ea360753fd208a70641df35728db1523df25
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-24 12:06:29 +0100

    Fix padding bug on track

--- src/components/Elements/Basic.jsx
+++ src/components/Elements/Basic.jsx
@@ -1,7 +1,11 @@
 import React, { PropTypes } from 'react'
 
 const Basic = ({ title, style }) =>
-  <div className="element" style={style}>{ title }</div>
+  <div className="element" style={style}>
+    <div className="element__content">
+      { title }
+    </div>
+  </div>
 
 Basic.propTypes = {
   title: PropTypes.string,
