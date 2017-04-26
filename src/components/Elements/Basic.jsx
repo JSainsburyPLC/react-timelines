ee97750b32fa5dd37a325dcc49df5f5085cee2b8
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-26 09:40:25 +0100

    Add tooltip description and fix transition bug.

--- src/components/Elements/Basic.jsx
+++ src/components/Elements/Basic.jsx
@@ -2,15 +2,23 @@ import React, { PropTypes } from 'react'
 
 import { getDayMonth } from '../../utils/formatDate'
 
-const Basic = ({ title, start, end, style }) =>
+const Basic = ({ title, start, end, style, tooltip }) =>
   <div className="element" style={style}>
     <div className="element__content" aria-hidden="true">
       { title }
     </div>
     <div className="element__tooltip">
-      <div>{title}</div>
-      <div><strong>Start</strong> {getDayMonth(start)}</div>
-      <div><strong>End</strong> {getDayMonth(end)}</div>
+      {
+        tooltip
+        ? <div>{tooltip}</div>
+        : (
+          <div>
+            <div>{title}</div>
+            <div><strong>Start</strong> {getDayMonth(start)}</div>
+            <div><strong>End</strong> {getDayMonth(end)}</div>
+          </div>
+        )
+      }
     </div>
   </div>
 
@@ -18,7 +26,8 @@ Basic.propTypes = {
   title: PropTypes.string,
   start: PropTypes.instanceOf(Date),
   end: PropTypes.instanceOf(Date),
-  style: PropTypes.shape({})
+  style: PropTypes.shape({}),
+  tooltip: PropTypes.string
 }
 
 export default Basic
--- src/components/Timeline/Tracks/Element.jsx
+++ src/components/Timeline/Tracks/Element.jsx
@@ -1,13 +1,14 @@
 import React, { PropTypes } from 'react'
 import BasicElement from '../../Elements/Basic'
 
-const Element = ({ time, trackStyle, style, id, title, start, end }) =>
+const Element = ({ time, trackStyle, style, id, title, start, end, tooltip }) =>
   <div
     key={id}
     className="track__element"
     style={time.toStyleLeftAndWidth(start, end)}
   >
     <BasicElement
+      tooltip={tooltip}
       title={title}
       start={start}
       end={end}
@@ -22,7 +23,8 @@ Element.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string,
   start: PropTypes.instanceOf(Date).isRequired,
-  end: PropTypes.instanceOf(Date).isRequired
+  end: PropTypes.instanceOf(Date).isRequired,
+  tooltip: PropTypes.string
 }
 
 export default Element
