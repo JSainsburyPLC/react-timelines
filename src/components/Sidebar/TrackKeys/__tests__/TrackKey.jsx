359a34a363ad8ef0f7a12717bfcce0acaa1541ee
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-27 15:54:52 +0100

    Fix TrackKey

--- src/components/Sidebar/TrackKeys/__tests__/TrackKey.jsx
+++ src/components/Sidebar/TrackKeys/__tests__/TrackKey.jsx
@@ -4,63 +4,56 @@ import { shallow } from 'enzyme'
 import TrackKey from '../TrackKey'
 import TrackKeys from '../'
 
-const createProps = ({
-  track = {
-    title: 'TEST'
-  },
-  toggleOpen = jest.fn()
-}) => ({ track, toggleOpen })
-
 describe('<TrackKey />', () => {
   describe('toggle button', () => {
     const getToggleButton = node => node.find('.track-key__toggle')
 
     it('renders when "track.isOpen" is defined', () => {
-      const track = {
-        title: 'TEST',
-        isOpen: false
+      const props = {
+        track: { title: 'test', isOpen: true },
+        toggleOpen: jest.fn()
       }
-      const props = createProps({ track })
       const wrapper = shallow(<TrackKey {...props} />)
       expect(getToggleButton(wrapper).exists()).toBe(true)
     })
 
     it('does not render when "track.isOpen" is undefined', () => {
-      const track = {
-        title: 'TEST'
+      const props = {
+        track: { title: 'test', isOpen: undefined },
+        toggleOpen: jest.fn()
       }
-      const props = createProps({ track })
       const wrapper = shallow(<TrackKey {...props} />)
       expect(getToggleButton(wrapper).exists()).toBe(false)
     })
 
     it('renders with the text "Close" when "track.isOpen" is "true"', () => {
-      const track = {
-        title: 'TEST',
-        isOpen: true
+      const props = {
+        track: { title: 'test', isOpen: true },
+        toggleOpen: jest.fn()
       }
-      const props = createProps({ track })
       const wrapper = shallow(<TrackKey {...props} />)
       expect(getToggleButton(wrapper).text()).toBe('Close')
     })
 
     it('renders with the text "Open" when "track.isOpen" is "false"', () => {
-      const track = {
-        title: 'TEST',
-        isOpen: false
+      const props = {
+        track: { title: 'test', isOpen: false },
+        toggleOpen: jest.fn()
       }
-      const props = createProps({ track })
       const wrapper = shallow(<TrackKey {...props} />)
       expect(getToggleButton(wrapper).text()).toBe('Open')
     })
 
     it('calls "toggleOpen()" when clicked passing "track" as a single argument', () => {
       const track = {
-        title: 'TEST',
+        title: 'test',
         isOpen: false
       }
       const toggleOpen = jest.fn()
-      const props = createProps({ track, toggleOpen })
+      const props = {
+        track,
+        toggleOpen
+      }
       const wrapper = shallow(<TrackKey {...props} />)
       getToggleButton(wrapper).simulate('click')
       expect(toggleOpen).toBeCalledWith(track)
@@ -69,44 +62,37 @@ describe('<TrackKey />', () => {
 
   describe('<TrackKeys />', () => {
     it('renders when "isOpen" is truthy and "tracks" is not empty', () => {
-      const track = {
-        title: 'TEST',
-        tracks: [{}],
-        isOpen: true
+      const props = {
+        track: { title: 'test', tracks: [{}], isOpen: true },
+        toggleOpen: jest.fn()
       }
-      const props = createProps({ track })
       const wrapper = shallow(<TrackKey {...props} />)
       expect(wrapper.find(TrackKeys).exists()).toBe(true)
     })
 
     it('does not render when "isOpen" is falsy', () => {
-      const track = {
-        title: 'TEST',
-        tracks: [{}],
-        isOpen: false
+      const props = {
+        track: { title: 'test', tracks: [{}], isOpen: false },
+        toggleOpen: jest.fn()
       }
-      const props = createProps({ track })
       const wrapper = shallow(<TrackKey {...props} />)
       expect(wrapper.find(TrackKeys).exists()).toBe(false)
     })
 
     it('does not render when "tracks" is falsy', () => {
-      const track = {
-        title: 'TEST',
-        isOpen: true
+      const props = {
+        track: { title: 'test', tracks: null, isOpen: true },
+        toggleOpen: jest.fn()
       }
-      const props = createProps({ track })
       const wrapper = shallow(<TrackKey {...props} />)
       expect(wrapper.find(TrackKeys).exists()).toBe(false)
     })
 
     it('does not render when "tracks" is an empty array', () => {
-      const track = {
-        title: 'TEST',
-        tracks: [],
-        isOpen: true
+      const props = {
+        track: { title: 'test', tracks: [], isOpen: true },
+        toggleOpen: jest.fn()
       }
-      const props = createProps({ track })
       const wrapper = shallow(<TrackKey {...props} />)
       expect(wrapper.find(TrackKeys).exists()).toBe(false)
     })
