dd3f8aa3de6722e2bac6a4bfcb15bc1885da81fc
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-27 13:58:22 +0100

    Test TrackKey

---
+++ src/components/Sidebar/TrackKeys/__tests__/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import TrackKeys from '../'
+import TrackKey from '../TrackKey'
+
+describe('<TrackKeys />', () => {
+  it('renders a <TrackKey /> for each track', () => {
+    const props = {
+      tracks: [{
+        id: '1',
+        title: 'Track 1'
+      }, {
+        id: '2',
+        title: 'Track 2'
+      }],
+      toggleOpen: jest.fn()
+    }
+    const wrapper = shallow(<TrackKeys {...props} />)
+    expect(wrapper.find(TrackKey)).toHaveLength(2)
+  })
+})
