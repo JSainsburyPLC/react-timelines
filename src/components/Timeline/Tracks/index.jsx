d5cb03c55c6b30fdbc5482ad1f49f8be4c4e1b86
Will Bamford <w.bamford@gmail.com>
2017-04-27 10:59:51 +0100

    Remove ability for element to inherit track styling (for now)

--- src/components/Timeline/Tracks/index.jsx
+++ src/components/Timeline/Tracks/index.jsx
@@ -4,14 +4,13 @@ import Track from './Track'
 const Tracks = ({ time, tracks }) =>
   <div className="tracks">
     {
-      tracks.map(({ id, elements, isOpen, tracks: children, style }) =>
+      tracks.map(({ id, elements, isOpen, tracks: children }) =>
         <Track
           key={id}
           time={time}
           elements={elements}
           isOpen={isOpen}
           tracks={children}
-          style={style}
         />
       )
     }
