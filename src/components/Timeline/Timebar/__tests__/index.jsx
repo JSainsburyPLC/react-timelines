d5c6cf182a15511852bd78f4d8ef86aea305d876
Robert Smith <rbrtsmith84@googlemail.com>
2017-04-28 08:55:34 +0100

    Test Timebar

---
+++ src/components/Timeline/Timebar/__tests__/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import Timebar from '../'
+import Row from '../Row'
+
+describe('<Timebar />', () => {
+  it('renders <Row /> components', () => {
+    const props = {
+      time: {},
+      rows: [
+        { id: '1', cells: [] },
+        { id: '1', cells: [] }
+      ]
+    }
+    const wrapper = shallow(<Timebar {...props} />)
+    expect(wrapper.find(Row)).toHaveLength(2)
+  })
+})
