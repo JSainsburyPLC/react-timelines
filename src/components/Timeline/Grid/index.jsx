dece82bf4577b9d29d11a49380d58834ac785b9b
Robert Smith <rbrtsmith84@googlemail.com>
2017-06-09 10:28:09 +0100

    refactor

--- src/components/Timeline/Grid/index.jsx
+++ src/components/Timeline/Grid/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Cell from './Cell'
-
 const VerticalGrid = ({ time, cells }) =>
   <div className="rt-grid">
-    {cells.map(({ id, start, end }) => <Cell key={id} time={time} start={start} end={end} />)}
+    {cells.map(({ id, start, end }) =>
+      <div key={id} className="rt-grid__cell" style={time.toStyleLeftAndWidth(start, end)} />)}
   </div>
 
 VerticalGrid.propTypes = {
   time: PropTypes.shape({}).isRequired,
-  cells: PropTypes.arrayOf(PropTypes.shape({})).isRequired
+  cells: PropTypes.arrayOf(PropTypes.shape({
+    start: PropTypes.instanceOf(Date).isRequired,
+    end: PropTypes.instanceOf(Date).isRequired
+  })).isRequired
 }
 
 export default VerticalGrid
